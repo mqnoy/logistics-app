2075f7343bdd55c45f4ed7fa2e124c7469292454
mqnoy <qnoy.rifky@gmail.com>
2024-07-03 01:31:49 +0700

    feat(be/user): Add login api

--- core/user/delivery/http/user_handler.go
+++ core/user/delivery/http/user_handler.go
@@ -25,6 +25,7 @@ func New(mux *chi.Mux, userUseCase domain.UserUseCase) {
 
 	mux.Route("/users", func(r chi.Router) {
 		r.Post("/register", handler.Register)
+		r.Post("/login", handler.Login)
 	})
 }
 
@@ -45,4 +46,21 @@ func (h userHandler) Register(w http.ResponseWriter, r *http.Request) {
 
 	handler.ParseResponse(w, r, "Register", result, err)
 }
+
+func (h userHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var request dto.LoginRequest
+	if err := render.Bind(r, &request); err != nil {
+		handler.ParseResponse(w, r, "", nil, cerror.WrapError(http.StatusBadRequest, err))
+		return
+	}
+
+	// Validate payload
+	if err := cvalidator.ValidateStruct(&request); err != nil {
+		handler.ParseToErrorValidation(w, r, http.StatusBadRequest, cvalidator.ErrorValidator, err)
+		return
+	}
+
+	result, err := h.userUseCase.LoginUser(request)
+
+	handler.ParseResponse(w, r, "Login", result, err)
 }
