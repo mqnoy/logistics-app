0353c5e92eab211fed0b37dc377dedb4f61e5931
mqnoy <qnoy.rifky@gmail.com>
2024-07-03 20:35:48 +0700

    fix(fe/component): Adjustment navbar component

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,36 +1,53 @@
 import { FC } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { cookieUtils } from '../utils'
+import { cookieUtils, toastUtils } from '../utils'
 
 export const Navbar: FC = () => {
     const navigate = useNavigate()
 
     const handleLogout = () => {
         cookieUtils.destroyCredentials()
-        navigate(`/login`, { replace: true })
-        location.reload()
+        toastUtils.fireToastSuccess("Logout successfully", {
+            onClose: () => {
+                navigate(`/login`, { replace: true })
+                location.reload()
+            }
+        })
     }
 
     return (
-        <>
-            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <NavLink className="navbar-item" to={'/'}>
-                            Dashboard
-                        </NavLink>
-                    </div>
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <button className="button is-light" onClick={handleLogout}>
-                                    Logout
-                                </button>
-                            </div>
+        <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <NavLink className="navbar-item" to={'/'}>
+                        Dashboard
+                    </NavLink>
+                    <NavLink className="navbar-item" to={'/goods'}>
+                        Goods
+                    </NavLink>
+                    <NavLink className="navbar-item" to={'/orders'}>
+                        Orders
+                    </NavLink>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <button className="button" onClick={handleLogout}>
+                                Logout
+                            </button>
                         </div>
                     </div>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     )
 }
