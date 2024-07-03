47807a48c77879b088284aebfbeb5dcf42053940
mqnoy <qnoy.rifky@gmail.com>
2024-07-04 01:05:39 +0700

    feat(fe/good): Implement update goods

--- webapp/src/api/goodsApi.ts
+++ webapp/src/api/goodsApi.ts
@@ -1,5 +1,14 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
-import { GoodCreateRequest, GoodCreateResponse, GoodsListResponse, ListRequest } from '../types'
+import {
+    BaseResponse,
+    GoodCreateRequest,
+    GoodCreateResponse,
+    GoodUpdateRequest,
+    GoodUpdateResponse,
+    Goods,
+    GoodsListResponse,
+    ListRequest,
+} from '../types'
 import { baseQuery } from '.'
 
 export const goodsApi = createApi({
@@ -19,8 +28,25 @@ export const goodsApi = createApi({
                 body: body,
             }),
         }),
+        getDetailGood: builder.query<BaseResponse<Goods>, string>({
+            query: (id) => ({
+                url: `/goods/${id}`,
+            }),
+        }),
+        putUpdateGood: builder.mutation<GoodUpdateResponse, GoodUpdateRequest>({
+            query: (body) => ({
+                url: `/goods/${body.id}`,
+                method: 'PUT',
+                body: body,
+            }),
+        }),
     }),
 })
 
-export const { useGetListGoodsQuery, useLazyGetListGoodsQuery, usePostCreateGoodMutation } =
-    goodsApi
+export const {
+    useGetListGoodsQuery,
+    useLazyGetListGoodsQuery,
+    usePostCreateGoodMutation,
+    useLazyGetDetailGoodQuery,
+    usePutUpdateGoodMutation,
+} = goodsApi
