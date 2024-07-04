9e798071e3c5042b97cfb90b094256dfb5af3bd8
mqnoy <qnoy.rifky@gmail.com>
2024-07-04 14:24:41 +0700

    chore(fe/utils): Add converter function

--- webapp/src/utils/date.utils.ts
+++ webapp/src/utils/date.utils.ts
@@ -1,5 +1,21 @@
-import { DateTime } from "luxon"
+import { DateTime } from 'luxon'
 
 export const epochToDateTime = (e: number): DateTime => {
     return DateTime.fromSeconds(e)
+}
+
+export const getCurrentTz = (): string => {
+    const { timeZone } = Intl.DateTimeFormat().resolvedOptions()
+    return timeZone
+}
+
+export const epochNumberToDateTimeStr = (e: number): string => {
+    const timeZone = getCurrentTz()
+    const dt = DateTime.fromSeconds(e)
+    dt.setZone(timeZone)
+    return dt.toFormat('MM-dd-yyyy HH:mm:ss')
+}
+
+export const dateToEpoch = (d: Date): number => {
+    return Math.floor(DateTime.fromJSDate(d).toMillis() / 1000)
 }
