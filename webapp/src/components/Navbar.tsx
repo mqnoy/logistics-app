8d6ab6ed1b28b00f2e2f21c9bfd59b4de71f12c8
mqnoy <qnoy.rifky@gmail.com>
2024-07-04 17:33:14 +0700

    fix(fe): Adjustment layout

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -17,36 +17,39 @@ export const Navbar: FC = () => {
 
     return (
         <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
-                    <NavLink className="navbar-item" to={'/'}>
-                        Dashboard
-                    </NavLink>
-                    <NavLink className="navbar-item" to={'/goods'}>
-                        Goods
-                    </NavLink>
-                    <NavLink className="navbar-item" to={'/orders'}>
-                        Orders
-                    </NavLink>
+            <div className="container">
+                <div className="navbar-brand">
+                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
 
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <button className="button" onClick={handleLogout}>
-                                Logout
-                            </button>
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <NavLink className="navbar-item has-text-white" to={'/'}>
+                            Dashboard
+                        </NavLink>
+                        <NavLink className="navbar-item has-text-white" to={'/goods'}>
+                            Goods
+                        </NavLink>
+                        <NavLink className="navbar-item has-text-white" to={'/orders'}>
+                            Orders
+                        </NavLink>
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <button className="button" onClick={handleLogout}>
+                                    Logout
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </nav>
     )
