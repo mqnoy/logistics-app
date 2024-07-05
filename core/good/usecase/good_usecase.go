b5f8f82e2be9111e791bde11e7c03b541cc9e483
mqnoy <qnoy.rifky@gmail.com>
2024-07-05 21:04:32 +0700

    feat(be/good): Add another decrease func v2

--- core/good/usecase/good_usecase.go
+++ core/good/usecase/good_usecase.go
@@ -348,3 +348,38 @@ func (u *goodUseCase) CheckAvailabilityStock(stock int, stockOut int) error {
 
 	return nil
 }
+
+func (u *goodUseCase) DecreaseStockV2(ctx context.Context, param dto.UpdateParam[dto.GoodStockRequest]) error {
+	// acquire transaction on context
+	trx := ctx.Value(constant.TrxKey).(*gorm.DB)
+
+	updateValue := param.UpdateValue
+	goodId := param.ID
+
+	// determine stock via good
+	row, err := u.goodRepo.SelectGoodById(goodId)
+	if err != nil {
+		// rollback transaction
+		u.txManager.CommitOrRollback(ctx, trx, true)
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return cerror.WrapError(http.StatusNotFound, fmt.Errorf("resource not found"))
+		}
+
+		log.Println(err)
+		return cerror.WrapError(http.StatusInternalServerError, fmt.Errorf("internal server error"))
+	}
+
+	if err := u.goodRepo.WithTrx(trx).UpdateGoodStockByGoodId(goodId, map[string]interface{}{
+		"total": row.GoodStock.Total - updateValue.Total,
+	}); err != nil {
+		log.Println(err)
+
+		// rollback transaction
+		u.txManager.CommitOrRollback(ctx, trx, true)
+
+		return cerror.WrapError(http.StatusInternalServerError, fmt.Errorf("internal server error"))
+	}
+
+	return nil
+}
