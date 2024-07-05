983e56fe2a48cfbb5945fecad236ec6b5a93ffc8
mqnoy <qnoy.rifky@gmail.com>
2024-07-05 21:06:27 +0700

    feat(be/order): Add Multiple Order Item in or out API

--- core/dto/order.go
+++ core/dto/order.go
@@ -1,6 +1,10 @@
 package dto
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mqnoy/logistics-app/core/enum"
+)
 
 type GoodOrderResponse struct {
 	Code string `json:"code"`
@@ -36,4 +40,46 @@ func (t *OrderInRequest) Bind(r *http.Request) error {
 type FilterOrderParams struct {
 	RequestAt []int64
 	OrderType int
+}
+
+type OrderCreateMultipleRequest struct {
+	Items []OrderItem `json:"items" validate:"required,unique"`
+	Type  enum.OrderType
+}
+
+// Bind implements render.Binder.
+func (o *OrderCreateMultipleRequest) Bind(r *http.Request) error {
+	return nil
+}
+
+type OrderItem struct {
+	Code  string `json:"code" validate:"min=1,max=10,required"`
+	Total int    `json:"total" validate:"min=1,number,required"`
+}
+
+type OrderItemTemp struct {
+	Code   string
+	GoodID string
+	Total  int
+	Reason error
+}
+
+type OrderItemResponse struct {
+	ID                   string       `json:"id"`
+	GoodSnapshotResponse GoodResponse `json:"good_snapshot"`
+	Total                int          `json:"total"`
+	Timestamp
+}
+
+type OrderItemMultipleResponse struct {
+	Code   string `json:"code"`
+	Total  int    `json:"total"`
+	Reason string `json:"reason"`
+}
+
+type OrderCreateMultipleResponse struct {
+	ID      string                      `json:"id"`
+	Success []OrderItemMultipleResponse `json:"success"`
+	Failed  []OrderItemMultipleResponse `json:"failed"`
+	Timestamp
 }
--- core/order/usecase/order_usecase.go
+++ core/order/usecase/order_usecase.go
@@ -237,3 +237,201 @@ func (u *orderUseCase) DetailOrder(param dto.DetailParam) (resp dto.OrderRespons
 
 	return resp, nil
 }
+
+func (u *orderUseCase) MultipleOrderInOut(ctx context.Context, param dto.CreateParam[dto.OrderCreateMultipleRequest]) (resp dto.OrderCreateMultipleResponse, err error) {
+	createValue := param.CreateValue
+
+	// perform with transaction
+	trx := u.txManager.AcquireTx(ctx)
+	ctx = context.WithValue(ctx, constant.TrxKey, trx)
+
+	var successOrderItems []dto.OrderItemTemp
+	var failedOrderItems []dto.OrderItemTemp
+
+	// persist insert order
+	orderRow, err := u.orderRepo.WithTrx(trx).InsertOrder(model.Order{
+		RequestAt: util.GetCurrentTime(),
+		Type:      int(createValue.Type),
+	})
+	if err != nil {
+		// rollback transaction
+		u.txManager.CommitOrRollback(ctx, trx, true)
+
+		log.Println(err)
+		return resp, cerror.WrapError(http.StatusInternalServerError, fmt.Errorf("internal server error"))
+	}
+
+	var orderItemRows []*model.OrderItem
+	orderId := orderRow.UUIDColumn.ID
+	for _, orderItem := range createValue.Items {
+		// determine goods by code
+		goodSnapshot, goodRow, err := u.goodUseCase.SnapshotGood(orderItem.Code)
+		if err != nil {
+			log.Println(err)
+
+			failedOrderItems = append(failedOrderItems, dto.OrderItemTemp{
+				Code:   orderItem.Code,
+				GoodID: "",
+				Total:  orderItem.Total,
+				Reason: err,
+			})
+
+			continue
+		}
+
+		// check barang should active
+		if !goodRow.IsActive {
+			failedOrderItems = append(failedOrderItems, dto.OrderItemTemp{
+				Code:   orderItem.Code,
+				GoodID: goodRow.ID,
+				Total:  orderItem.Total,
+				Reason: fmt.Errorf("resource inactive"),
+			})
+			continue
+		}
+
+		// check stock if order is type out
+		if createValue.Type == enum.ORDER_OUT {
+			if err := u.goodUseCase.CheckAvailabilityStock(goodRow.GoodStock.Total, orderItem.Total); err != nil {
+				failedOrderItems = append(failedOrderItems, dto.OrderItemTemp{
+					Code:   orderItem.Code,
+					GoodID: goodRow.ID,
+					Total:  orderItem.Total,
+					Reason: fmt.Errorf("out of stock"),
+				})
+				continue
+			}
+		}
+
+		goodsId := goodRow.ID
+
+		// persist insert orderItem
+		itemRow, err := u.orderRepo.WithTrx(trx).InsertOrderItem(model.OrderItem{
+			OrderID: orderId,
+			Total:   orderItem.Total,
+			GoodID:  goodRow.ID,
+			GoodSnapShotColumn: model.GoodSnapShotColumn{
+				GoodSnapShot: goodSnapshot.Snapshot,
+			},
+		})
+		if err != nil {
+			log.Println(err)
+
+			failedOrderItems = append(failedOrderItems, dto.OrderItemTemp{
+				Code:   orderItem.Code,
+				GoodID: "",
+				Total:  orderItem.Total,
+				Reason: err,
+			})
+			continue
+		}
+		orderItemRows = append(orderItemRows, itemRow)
+
+		// append success order
+		successOrderItems = append(successOrderItems, dto.OrderItemTemp{
+			Code:   orderItem.Code,
+			GoodID: goodsId,
+			Total:  orderItem.Total,
+			Reason: nil,
+		})
+	}
+
+	// call goodUseCase for increasing/decrease stock
+	if createValue.Type == enum.ORDER_IN {
+		if err := u.OrderTypeInFn(ctx, successOrderItems); err != nil {
+			return resp, err
+		}
+	} else if createValue.Type == enum.ORDER_OUT {
+		if err := u.OrderTypeOutFn(ctx, successOrderItems); err != nil {
+			return resp, err
+		}
+	} else {
+		// rollback transaction
+		u.txManager.CommitOrRollback(ctx, trx, true)
+
+		return resp, cerror.WrapError(http.StatusBadRequest, fmt.Errorf("error determine order type"))
+	}
+
+	// update count_item
+	u.orderRepo.WithTrx(trx).UpdateOrderById(orderRow.ID, map[string]interface{}{
+		"count_item": len(successOrderItems),
+	})
+
+	// commit transaction
+	if len(successOrderItems) != 0 {
+		u.txManager.CommitOrRollback(ctx, trx, false)
+	} else {
+		u.txManager.CommitOrRollback(ctx, trx, true)
+	}
+
+	// compose response
+	orderRow.OrderItem = orderItemRows
+	return u.ComposeOrderCreateMultiple(orderRow, successOrderItems, failedOrderItems), nil
+}
+
+func (u *orderUseCase) OrderTypeInFn(ctx context.Context, successOrderItems []dto.OrderItemTemp) (err error) {
+	for _, orderItem := range successOrderItems {
+		if err := u.goodUseCase.IncreaseStock(ctx, dto.UpdateParam[dto.GoodStockRequest]{
+			ID: orderItem.GoodID,
+			UpdateValue: dto.GoodStockRequest{
+				Total: orderItem.Total,
+			},
+		}); err != nil {
+			// rollback transaction on IncreaseStock
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (u *orderUseCase) OrderTypeOutFn(ctx context.Context, successOrderItems []dto.OrderItemTemp) (err error) {
+	for _, orderItem := range successOrderItems {
+		if err := u.goodUseCase.DecreaseStockV2(ctx, dto.UpdateParam[dto.GoodStockRequest]{
+			ID: orderItem.GoodID,
+			UpdateValue: dto.GoodStockRequest{
+				Total: orderItem.Total,
+			},
+		}); err != nil {
+			// rollback transaction on IncreaseStock
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (u *orderUseCase) ComposeOrderCreateMultiple(m *model.Order, success, failed []dto.OrderItemTemp) dto.OrderCreateMultipleResponse {
+	var successArr = make([]dto.OrderItemMultipleResponse, len(success))
+	var reason string
+	for i, s := range success {
+		if s.Reason != nil {
+			reason = s.Reason.Error()
+		}
+		successArr[i] = dto.OrderItemMultipleResponse{
+			Code:   s.Code,
+			Total:  s.Total,
+			Reason: reason,
+		}
+	}
+
+	var failedArr = make([]dto.OrderItemMultipleResponse, len(failed))
+	for i, s := range failed {
+		if s.Reason != nil {
+			reason = s.Reason.Error()
+		}
+
+		failedArr[i] = dto.OrderItemMultipleResponse{
+			Code:   s.Code,
+			Total:  s.Total,
+			Reason: reason,
+		}
+	}
+
+	return dto.OrderCreateMultipleResponse{
+		ID:        m.ID,
+		Success:   successArr,
+		Failed:    failedArr,
+		Timestamp: dto.ComposeTimestamp(m.TimestampColumn),
+	}
+}
